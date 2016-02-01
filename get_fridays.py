b781746ebd55c89b19cf88c8bba2dcf5ed19b034
Andres Marrugo <agmarrugo@gmail.com>
2016-02-01 13:29:47 -0500

    Fixed flake8 errors.

--- get_fridays.py
+++ get_fridays.py
@@ -12,19 +12,20 @@ from tabulate import tabulate
 c = calendar.Calendar(firstweekday=calendar.MONDAY)
 
 year = 2016
-months = [2,3,4,5]
-monthName = {k: v for k,v in enumerate(calendar.month_abbr)}
+months = [2, 3, 4, 5]
+monthName = {k: v for k, v in enumerate(calendar.month_abbr)}
 
 listFridays = []
 for month in months:
-    
-    monthcal = c.monthdatescalendar(year,month)  
-    
+
+    monthcal = c.monthdatescalendar(year, month)
+
     for week in monthcal:
         for day in week:
             if day.weekday() == calendar.FRIDAY and day.month == month:
-                listFridays.append([monthName[day.month]+" "+str(day.day)," "," "])
-                
-headers = ["Date","Lecture","Lecture Topics"] 
+                listFridays.append(
+                    [monthName[day.month] + " " + str(day.day), " ", " "])
 
-print tabulate(listFridays,headers,tablefmt='pipe')
+headers = ["Date", "Lecture", "Lecture Topics"]
+
+print tabulate(listFridays, headers, tablefmt='pipe')
